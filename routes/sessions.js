5bcec74079fbdf6ae3ae86ad3367946c752680be
lengsel77 <s.cunningham777@gmail.com>
2014-11-04 22:43:24 -0600

    Style updates

--- routes/sessions.js
+++ routes/sessions.js
@@ -13,9 +13,6 @@
 exports.list = function(req, res) {
 	var db = req.db;
 	var skillName = req.params.skillname;
-	for (var i in db) {
-    	console.info(i.toString());
-    }
 
 	db.collection('usercollection').findOne({_id: db.toObjectID('544d8ff19216375f8f23fade'), 'skills.name': skillName}, {'skills.$': 1}, function(err, result) {
 		res.json(result.skills[0].sessions);
@@ -29,12 +26,7 @@ exports.add = function(req, res) {
     var db = req.db;
     var skillName = req.params.skillname;
     var userId = '544d8ff19216375f8f23fade';
-    console.info('Add session');
-/*    for (var i in db) {
-    	console.info(i.toString());
-    }
-*/    db.collection('usercollection').update({_id: db.toObjectID(userId), 'skills.name': skillName}, {'$push': {"skills.$.sessions": req.body}}, function(err, result){
-    	console.info(db.toString());
+    db.collection('usercollection').update({_id: db.toObjectID(userId), 'skills.name': skillName}, {'$push': {"skills.$.sessions": req.body}}, function(err, result){
         res.send(
             (err === null) ? { msg: '' } : { msg: err }
         );
