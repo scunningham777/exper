9540533f780efaa9f7188e375396cb9efc5acef1
scunningham777 <s.cunningham777@gmail.com>
2015-03-29 22:31:03 -0500

    Skill list view looking good
    good amount of work on the skill list view - nothing does anything yet,
    but it will.

--- server.js
+++ server.js
@@ -33,8 +33,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.set('partials', path.join(__dirname, 'partials'));
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
