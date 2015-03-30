fb14aebe286de1ad7058f366e07ea708384c8a06
scunningham777 <s.cunningham777@gmail.com>
2015-03-29 22:56:48 -0500

    More work on Skills view
    Should have method stubs for everything now.

--- public/javascripts/controllers/mxSkillsCtrl.js
+++ public/javascripts/controllers/mxSkillsCtrl.js
@@ -1,8 +1,36 @@
 'use strict';
 
 application.controller('mxSkillsCtrl', function($scope, mxSkill) {
+	$scope.data = {
+		showDelete:false,
+		showReorder:false
+	};
+
 	$scope.skills = mxSkill.listWithDuration();
-	$scope.goToAddSession = function(skill) {
+
+	$scope.gotoAddSession = function(skill) {
 		console.log("goToAddSession for skill " + skill.name);
-	}
+	};
+
+	$scope.gotoAddSkill = function() {
+		console.log("gotoAddSkill");
+	};
+
+	$scope.deleteSkill = function(skill) {
+		if (confirm("Are you sure you want to delete Skill \"" + skill.name + "\"?")) {
+			console.log("delete skill " + skill.name)
+		};		
+	};
+
+	$scope.editSkill = function(skill) {
+		console.log("Edit skill " + skill.name);
+	};
+
+	$scope.moveItem = function(skill, fromIndex, toIndex) {
+		if(toIndex>fromIndex) toIndex-=1;
+    	$scope.skills.splice(fromIndex, 1);
+    	$scope.skills.splice(toIndex, 0, skill);
+    	//save skills back to database with updated order?
+    	console.log("Reordering won't persist!");
+  	};
 });
